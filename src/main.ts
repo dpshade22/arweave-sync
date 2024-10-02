61f792b107a2f8c9f50f6654611900a90339289a
dpshade22 <dpshadey22@gmail.com>
2024-10-02 15:40:27 -0400

    fix button styles and main.js

--- src/main.ts
+++ src/main.ts
@@ -291,6 +291,8 @@ export default class ArweaveSync extends Plugin {
 
   private createSyncButton() {
     const syncButton = document.createElement("button");
+    syncButton.addClass("clickable-icon");
+    syncButton.addClass("view-action");
     syncButton.addClass("arweave-sync-button");
     syncButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Sync current file to Arweave</title><path d="M12,18A6,6 0 0,1 6,12C6,11 6.25,10.03 6.7,9.2L5.24,7.74C4.46,8.97 4,10.43 4,12A8,8 0 0,0 12,20V23L16,19L12,15M12,4V1L8,5L12,9V6A6,6 0 0,1 18,12C18,13 17.75,13.97 17.3,14.8L18.76,16.26C19.54,15.03 20,13.57 20,12A8,8 0 0,0 12,4Z" /></svg>`;
     return syncButton;
@@ -446,7 +448,7 @@ export default class ArweaveSync extends Plugin {
       if (
         !this.settings.localUploadConfig[filePath] ||
         (fileInfo as FileUploadInfo).timestamp >
-          this.settings.localUploadConfig[filePath].timestamp
+        this.settings.localUploadConfig[filePath].timestamp
       ) {
         this.settings.localUploadConfig[filePath] = fileInfo as FileUploadInfo;
       }
